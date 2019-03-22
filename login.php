1a6bbf701d32e9f3444a275f8632f440863ee52a
kinaway <rogerio.gns@gmail.com>
2019-03-22 17:34:27 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -3,12 +3,15 @@ if($_SESSION){
     header("Location: index.php");
     exit();
 }
+if($_POST['LOGIN']){
+    require_once("functions/logar.php");
+}
 ?>
 <html>
     <body>
         <form>
-            <label>Login:<input type="text"></label>
-            <label>Senha:<input type="password"></label>
+            <label>Login:<input name="LOGIN" type="text"></label>
+            <label>Senha:<input name="PASSWORD" type="password"></label>
             <input type="submit" value="Entrar">
         </form>
     </body>
